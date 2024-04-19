221caf3156da6886c3bbb29b569fa38d3dc77957
qiufuyu123 <3038742090@qq.com>
2024-04-19 19:53:18 +0800

    fix debug ui unresizeable

--- emulator/casioemu.cpp
+++ emulator/casioemu.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <iterator>
 #include <ostream>
+#include <ratio>
 #include <thread>
 #include <string>
 #include <map>
@@ -160,26 +161,12 @@ int main(int argc, char *argv[])
 		// 	}
 		// });
 		DebugUi ui;
-		float m_DeltaTime;
-		Uint64 m_StartTime;
-		Uint64 m_PreFrameTime;
-		m_StartTime = SDL_GetPerformanceCounter();
-		m_PreFrameTime = m_StartTime;
+
 		std::thread uit([&]{
 			while (true) {
 				ui.PaintUi();
 				ui.PaintSDL();
-				Uint64 currTime = SDL_GetPerformanceCounter();
-				m_DeltaTime = (currTime - m_PreFrameTime) / (float)SDL_GetPerformanceFrequency();
-				m_PreFrameTime = currTime;
-				float totalTime = (currTime - m_StartTime) / (float)SDL_GetPerformanceFrequency();
-				float FPS = 1.0f / m_DeltaTime;
-				//SDL_LogInfo(0, "totalTime: %f\tdt: %f\tFPS: %f\n", totalTime, m_DeltaTime, FPS);
-
-				// 限制帧率
-				int MaxFPS = 120;
-				if (m_DeltaTime < 1.0f / MaxFPS * 1000.0f)
-					SDL_Delay(Uint32(floor(1.0 / MaxFPS * 1000.0 - m_DeltaTime)));
+				std::this_thread::sleep_for(std::chrono::milliseconds(16));
 			}
 			
 		});
@@ -218,7 +205,7 @@ int main(int argc, char *argv[])
 				break;
 
 			case SDL_WINDOWEVENT:
-				
+				ui.PaintSDL();
 				switch (event.window.event)
 				{
 				case SDL_WINDOWEVENT_CLOSE:
@@ -231,7 +218,7 @@ int main(int argc, char *argv[])
 					 	// send resized event, but some still does (such as xmonad)
 					 	break;
 					 }
-					ui.PaintSDL();
+					
 					//ImGui_ImplSDL2_ProcessEvent(&event);
 					if(event.window.windowID == SDL_GetWindowID(emulator.window)){
 					emulator.WindowResize(event.window.data1, event.window.data2);
